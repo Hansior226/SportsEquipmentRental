74aa7d4606cf89b16048e4813d320f0c85483c62
Hans <handzloikn@gmail.com>
2025-03-05 18:23:36 +0100

    Top row names changed
    First steps top row change

---
+++ SportsEquipmentRental/Controllers/EquipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentEquipment.Controllers
+{
+    public class SprzetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
